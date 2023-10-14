2174058662af67dced896f4092ccc114d72a99fe
abhi <abhinakka1912@gmail.com>
2023-10-14 14:35:42 +0530

    tiny updates
    - autoscroll in notes

--- src/frontend/Notes/Notes.jsx
+++ src/frontend/Notes/Notes.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Notes.css';
 
 function Notes({ markerNotes, hoveredMarkerId }) {
+  const listRef = useRef(null);
+
+  // Use the ref to scroll to the highlighted item when it changes
+  useEffect(() => {
+    if (hoveredMarkerId && listRef.current) {
+      const highlightedItem = listRef.current.querySelector('.highlighted');
+      if (highlightedItem) {
+        highlightedItem.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [hoveredMarkerId]);
+
   return (
     <div className="notes">
       <div className="notes-heading">Notes</div>
       <div className="note-list-container">
-        <ul className='note-items'>
+        <ul ref={listRef} className='note-items'>
           {Object.entries(markerNotes).map(([featureId, noteText]) => (
             <li key={featureId} className={featureId === hoveredMarkerId ? 'highlighted' : ''} > {noteText} </li>
           ))}
